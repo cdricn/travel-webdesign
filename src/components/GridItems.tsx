fb012bc8b4cee2c758a1d68ac012eec5d166e0ee
cdricn <cdricncdricn@gmail.com>
2024-08-28 20:50:35 +0800

    Added offset in json and before gradient

--- src/components/GridItems.tsx
+++ src/components/GridItems.tsx
@@ -7,7 +7,8 @@ function GridItems() {
       {data.content.map((item, index) => (
         <div className='grid_item' key={index} style={
           {
-            backgroundImage: `url("/${item.image}")`
+            backgroundImage: `url("/${item.image}")`,
+            backgroundPosition: `50% ${item.offset}%`
           }}>
           <div className='item_title'>
             <h2>{item.title}</h2>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import styles from './Navbar.module.css'
 
 function Navbar(){
+
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.button_container}>
+        <div className={styles.button_container} onClick={() => console.log('test')}>
           <div className={styles.button_line}></div>
           <div className={styles.button_line}></div>
           <div className={styles.button_line}></div>
